9a40e518e66db0b9cd7f6dd0229ec8b7fb2f549b
morgan_chester <morganchester45@gmail.com>
2025-07-27 03:03:48 +0500

    update; 4Octaves; ui updated;

--- ui/ui.go
+++ ui/ui.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -10,21 +11,36 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"gopkg.in/yaml.v3"
 	"kalterwind-mapper/midi"
 )
+
+type Mapping struct {
+	InputDevice  string      `yaml:"inputDevice"`
+	OutputDevice string      `yaml:"outputDevice"`
+	Octaves      interface{} `yaml:"octaves"`
+	Description  string      `yaml:"description"`
+	Mapping      struct {
+		NoteOn  map[int]int `yaml:"noteon"`
+		NoteOff string      `yaml:"noteoff"`
+	} `yaml:"mapping"`
+	Controls map[int]string `yaml:"controls"`
+}
 
 func RunApp() {
 	a := app.New()
 	w := a.NewWindow("Kalterwind Mapper (Go Edition)")
-	w.Resize(fyne.NewSize(500, 400))
+	w.Resize(fyne.NewSize(550, 500))
 
-	// Списки устройств
-	inputSelect := widget.NewSelect([]string{}, func(s string) {})
-	outputSelect := widget.NewSelect([]string{}, func(s string) {})
-	presetSelect := widget.NewSelect([]string{}, func(s string) {})
+	// UI элементы
+	inputSelect := widget.NewSelect([]string{}, nil)
+	outputSelect := widget.NewSelect([]string{}, nil)
+	presetSelect := widget.NewSelect([]string{}, nil)
+
+	infoLabel := widget.NewLabel("Информация о пресете появится здесь")
 
 	// Загрузка пресетов
-	presets := loadPresets("./presets")
+	presets, mappings := loadPresets("./presets")
 	if len(presets) == 0 {
 		presetSelect.Options = []string{"Нет доступных пресетов"}
 		presetSelect.Disable()
@@ -32,7 +48,21 @@ func RunApp() {
 		presetSelect.Options = presets
 	}
 
-	// Кнопка обновления списка устройств
+	// Выбор пресета → показать инфо
+	presetSelect.OnChanged = func(s string) {
+		if s == "" || s == "Нет доступных пресетов" {
+			infoLabel.SetText("Пресет не выбран")
+			return
+		}
+		m := mappings[s]
+		info := fmt.Sprintf(
+			"Описание: %s\nОктавы: %v\nInput: %s\nOutput: %s",
+			m.Description, m.Octaves, m.InputDevice, m.OutputDevice,
+		)
+		infoLabel.SetText(info)
+	}
+
+	// Кнопка обновления устройств
 	refreshBtn := widget.NewButton("Обновить устройства", func() {
 		ins, outs := midi.GetDevices()
 		if len(ins) == 0 {
@@ -71,6 +101,7 @@ func RunApp() {
 		}
 	})
 
+	// Контейнер
 	content := container.NewVBox(
 		widget.NewLabel("Входное устройство:"),
 		inputSelect,
@@ -78,6 +109,7 @@ func RunApp() {
 		outputSelect,
 		widget.NewLabel("Пресет:"),
 		presetSelect,
+		infoLabel,
 		refreshBtn,
 		startBtn,
 	)
@@ -86,13 +118,26 @@ func RunApp() {
 	w.ShowAndRun()
 }
 
-func loadPresets(path string) []string {
+func loadPresets(path string) ([]string, map[string]Mapping) {
 	var presets []string
+	mappings := make(map[string]Mapping)
+
 	_ = filepath.WalkDir(path, func(p string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
 		if !d.IsDir() && strings.HasSuffix(d.Name(), ".yaml") {
 			presets = append(presets, d.Name())
+
+			data, err := os.ReadFile(p)
+			if err == nil {
+				var m Mapping
+				if err := yaml.Unmarshal(data, &m); err == nil {
+					mappings[d.Name()] = m
+				}
+			}
 		}
 		return nil
 	})
-	return presets
+	return presets, mappings
 }
